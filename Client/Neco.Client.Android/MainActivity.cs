cc9fafdf3ef7f3265933414c0bc21ae17f22131d
momo5502 <mauriceheumann@gmail.com>
2017-11-04 14:42:46 +0100

    Add 'prototype-y' splash screen

--- Client/Neco.Client.Android/MainActivity.cs
+++ Client/Neco.Client.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace Neco.Client.Droid
 {
-	[Activity (Label = "NeCo", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "NeCo", Icon = "@drawable/icon", Theme="@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
---
+++ Client/Neco.Client.Android/StartActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace Neco.Client.Droid
+{
+    [Activity(Label = "NeCo", Icon = "@drawable/icon", Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class StartActivity : Activity
+    {
+        private bool resumed = false;
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            if (!resumed)
+            {
+                resumed = true;
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            }
+        }
+    }
+}
