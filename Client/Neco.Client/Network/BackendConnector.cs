e9b0e4aab17c1a0bb319ef2974dac5439e566e02
momo5502 <mail@momo5502.com>
2017-11-05 01:12:27 +0100

    Add backend connector prototype

---
+++ Client/Neco.Client/Network/BackendConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Neco.Client.Network
+{
+    public class BackendConnector
+    {
+        private TcpClient client;
+
+        public BackendConnector(String host)
+        {
+            var separator = host.LastIndexOf(':');
+            if (separator < 0) throw new ArgumentException();
+
+            int port = Convert.ToInt32(host.Substring(separator + 1));
+            String server = host.Substring(0, separator);
+
+            client = new TcpClient(server, port);
+        }
+
+        public void Send(String data)
+        {
+            Byte[] bytes = Encoding.ASCII.GetBytes(data);
+
+            NetworkStream stream = client.GetStream();
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        public void Receive()
+        {
+            // TODO: Use event handlers to dispatch incoming data
+        }
+    }
+}
