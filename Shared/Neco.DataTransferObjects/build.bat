5503ffe386ccd5631d53a6527491248cc30bb1f1
momo5502 <mauriceheumann@gmail.com>
2017-11-28 13:21:42 +0100

    Proto experiments

---
+++ Shared/Neco.DataTransferObjects/build.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"../../packages/Google.ProtocolBuffers.2.4.1.555/tools/ProtoGen.exe" Neco.proto -output_directory="C:\Users\mauri\Documents\Projects\NeCo\Shared\Neco.DataTransferObjects"
