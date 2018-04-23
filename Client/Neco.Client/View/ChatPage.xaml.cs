a8c288e28cf635dcef11e06018c4a212e8d23a48
Shiro_Orehara <shiro.orihara@gmail.com>
2018-04-23 11:54:47 +0200

    NECO-54 basic ui concept for file sharing

--- Client/Neco.Client/View/ChatPage.xaml.cs
+++ Client/Neco.Client/View/ChatPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Neco.DataTransferObjects;
+using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
 using System;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
@@ -68,6 +70,26 @@ namespace Neco.Client
             sendButton.Clicked += delegate
             {
                 SubmitMessage();
+            };
+
+            shareButton.Clicked += async (object sender, EventArgs args) =>
+            {
+                try
+                {
+                    FileData fileData = await CrossFilePicker.Current.PickFile();
+                    if (fileData == null)
+                        return; // user canceled file picking
+
+                    string fileName = fileData.FileName;
+                    string contents = System.Text.Encoding.UTF8.GetString(fileData.DataArray);
+
+                    System.Console.WriteLine("File name chosen: " + fileName);
+                    System.Console.WriteLine("File data: " + contents);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("Exception choosing file: " + ex.ToString());
+                }
             };
 
             viewModel.Messages.CollectionChanged += (object sender, NotifyCollectionChangedEventArgs e) =>
