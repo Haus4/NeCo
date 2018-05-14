2f80e378351d19bc4713967874b036be71b043cf
momo5502 <mauriceheumann@gmail.com>
2018-05-14 12:30:38 +0200

    Add more test methods and use correct unittest framework

--- Client/Neco.UnitTest/CryptoHandlerTest.cs
+++ Client/Neco.UnitTest/CryptoHandlerTest.cs
@@ -1,8 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Xunit;
+using System.Collections.Generic;
 
 namespace Neco.UnitTest
 {
+    [TestClass]
     public class CryptoHandlerTest
     {
         MockDataStore dataStore;
@@ -14,23 +16,55 @@ namespace Neco.UnitTest
             handler = new Client.Core.CryptoHandler(null, dataStore);
         }
 
-        [Fact]
+        [TestMethod]
         public void ShouldGenerateKeys()
         {
-            Assert.NotNull(dataStore.GetString(null, "publicKey"));
-            Assert.NotNull(dataStore.GetString(null, "privateKey"));
+            Assert.IsNotNull(dataStore.GetString(null, "publicKey"));
+            Assert.IsNotNull(dataStore.GetString(null, "privateKey"));
         }
 
-        [Fact]
-        public void ShouldNotGenerateKeysIfStored()
+        [TestMethod]
+        public void ShouldRegenerateKeysIfInvalid()
         {
-            dataStore = new MockDataStore();
             dataStore.SetString(null, "publicKey", "test");
             dataStore.SetString(null, "privateKey", "test");
             handler = new Client.Core.CryptoHandler(null, dataStore);
 
-            Assert.Equal("test", dataStore.GetString(null, "publicKey"));
-            Assert.Equal("test", dataStore.GetString(null, "privateKey"));
+            Assert.AreNotEqual("test", dataStore.GetString(null, "publicKey"));
+            Assert.AreNotEqual("test", dataStore.GetString(null, "privateKey"));
+        }
+
+        [TestMethod]
+        public void ShouldVeriySignature()
+        {
+            var signature = handler.CalculateSignature("test");
+
+            Assert.IsTrue(handler.VerifySignature("test", signature));
+        }
+
+        [TestMethod]
+        public void ShouldFailVerifyingSignature()
+        {
+            var signature = handler.CalculateSignature("test");
+            var signature2 = handler.CalculateSignature("test");
+            signature2[0]++;
+
+            Assert.IsFalse(handler.VerifySignature("test1", signature));
+            Assert.IsFalse(handler.VerifySignature("test", signature2));
+        }
+
+        [TestMethod]
+        public void ShouldVerifySecuritySignature()
+        {
+            var signature = handler.CalculateSecuritySignature();
+
+            Assert.IsTrue(handler.VerifySecuritySignature(signature));
+        }
+
+        [TestMethod]
+        public void ShouldSerializePublicKey()
+        {
+            Assert.IsNotNull(handler.SerializePublicKey());
         }
     }
 }
