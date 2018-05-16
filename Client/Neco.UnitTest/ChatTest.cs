3581e6bb21c062aa41f1d711015edfb3fbaab74b
momo5502 <mauriceheumann@gmail.com>
2018-05-16 08:27:51 +0200

    Add template tests for further use cases

---
+++ Client/Neco.UnitTest/ChatTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neco.UnitTest
+{
+    [TestClass]
+    public class ChatTest
+    {
+        [TestMethod]
+        public void ShouldSendMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ShouldSendImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ShouldReceiveMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ShouldReceiveImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
